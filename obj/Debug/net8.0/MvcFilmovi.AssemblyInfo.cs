7edf74c2cdb0667e1719581e4fbf3a9420833f17
Baliban Mario <mariobalibanos@gmail.com>
2024-06-11 15:42:05 +0200

    vjezbe

--- obj/Debug/net8.0/MvcFilmovi.AssemblyInfo.cs
+++ obj/Debug/net8.0/MvcFilmovi.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MvcFilmovi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+db6bec2c1ec5320f1bc7c49e4c70effacc2c6032")]
 [assembly: System.Reflection.AssemblyProductAttribute("MvcFilmovi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MvcFilmovi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
